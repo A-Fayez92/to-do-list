175f4b3ab6c4c9fd582a10bcde21b68c6adb85d4
Ahmed Fayez Salama <60694154+A-Fayez92@users.noreply.github.com>
2023-12-06 13:41:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,51 @@
-window.onload = function() {
-  console.log('window - onload'); // 4th
+function waitForElement(selector, callback) {
+    const targetNode = document;
 
-    var metaTags = document.getElementsByTagName('meta');
-    
-var filteredTags = Array.from(metaTags).filter(function(tag) {
+    const config = { childList: true, subtree: true };
 
-    var tagName = tag.getAttribute('name');
-    var tagContent = tag.getAttribute('content');
+    function elementFoundCallback(mutationsList, observer) {
+        const targetElement = document.querySelector(selector);
+        if (targetElement) {
+            observer.disconnect();
 
-    return tagName && tagContent && tagName === 'description' && tagContent === 'returns-portal-page';
-});
-   
-if(filteredTags.length > 0)
-{
+            callback(targetElement);
+        }
+    }
 
-    var subDomain = window.store_uuid;
-    
-    var iframeElement = document.createElement('iframe');
-    
-    iframeElement.src = 'https://' + subDomain + '.r.test'; 
-    iframeElement.style.minWidth = '100%';
-    iframeElement.style.minHeight = '100vh';
+    const observer = new MutationObserver(elementFoundCallback);
 
-    
-         var element = document.querySelector('.row');
-
-    if (element) {
-        element.appendChild(iframeElement);
-    } 
-
-    //public/zid/script.js
+    observer.observe(targetNode, config);
 }
-};
+
+waitForElement('.row', function (targetElement) {
+    var metaTags = document.getElementsByTagName('meta');
+
+    var filteredTags = Array.from(metaTags).filter(function (tag) {
+
+        var tagName = tag.getAttribute('name');
+        var tagContent = tag.getAttribute('content');
+
+        return tagName && tagContent && tagName === 'description' && tagContent === 'returns-portal-page';
+    });
+
+    if (filteredTags.length > 0) {
+
+        var subDomain = window.store_uuid;
+
+        var iframeElement = document.createElement('iframe');
+
+        iframeElement.src = 'https://' + subDomain + '.r.test';
+        iframeElement.style.minWidth = '100%';
+        iframeElement.style.minHeight = '100vh';
 
 
-    
+        var element = document.querySelector('.row');
 
+        if (element) {
+            element.appendChild(iframeElement);
+        }
 
+        //public/zid/script.js
+    }
+});
 
