c95d2511622254e4be59c56b16d30f84dfc1a993
Ahmed Fayez Salama <60694154+A-Fayez92@users.noreply.github.com>
2023-12-06 11:08:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+alert('hello');
+
+// (function(window , document) {
+
+// window.document.addEventListener('DOMContentLoaded', function() {
+//     alert('hello');
+// var metaTags = document.getElementsByTagName('meta');
+
+
+// var filteredTags = Array.from(metaTags).filter(function(tag) {
+
+//     var tagName = tag.getAttribute('name');
+//     var tagContent = tag.getAttribute('content');
+
+//     return tagName && tagContent && tagName === 'description' && tagContent === 'returns-portal-page';
+// });
+   
+
+// if(filteredTags.length > 0)
+// {
+
+//     var subDomain = window.store_uuid;
+//     var iframeElement = document.createElement('iframe');
+    
+//     iframeElement.src = 'https://' + subDomain + '.r.test'; 
+//     iframeElement.style.minWidth = '100%';
+//     iframeElement.style.minHeight = '100vh';
+
+
+//     var element = document.querySelector('.col-md-12');
+
+//     if (element) {
+//         nestedElement.appendChild(iframeElement);
+//     } 
+//     //public/zid/script.js
+// }
+
+// });
+// })(window, document);
+
